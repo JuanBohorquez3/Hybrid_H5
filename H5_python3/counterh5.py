4956712cafd8f888524d40678efccb6524beb675
JuanBohorquez3 <jbohorquez@wisc.edu>
2021-01-11 13:29:24 -0600

    Deal with h5 writing issues. Implement support for first-N-filter

--- H5_python3/counterh5.py
+++ H5_python3/counterh5.py
@@ -50,10 +50,12 @@ def load_data(
     else:
         shots_per_measurement = shots
 
-    binned_counter_data = np.zeros(
+    binned_counter_data = np.ones(
         (num_its, measurements, shots_per_measurement, drop_bins + ro_bins),
         dtype=int
-    )
+    )*-1
+
+    filter_offset = check_n_filter(results_file)
 
     for iteration, i_group in results_file['iterations'].items():
         # print(f"iteration : {iteration} : {type(iteration)}")
@@ -61,15 +63,51 @@ def load_data(
             # load data, a 1D array of counter data from the measurement, each entry corresponds to
             # the measured counts in a given binning period
             raw_data = np.array(m_group['data/counter/data'][()])[0]
+
+            # Loose cables or improper triggering can lead to some lost bins. We simply do not write
+            # this data to our data arrays
+            if raw_data.shape[0] != shots_per_measurement*(drop_bins+ro_bins):
+                inds = {
+                    "iteration": iteration,
+                    "measurement": measurement
+                }
+                ind_msg = ",".join([f"{var} : {val}" for var, val in inds.items()])
+                print(
+                    f"Invalid data shape in hdf5 file. raw_data.shape = {raw_data.shape}" +
+                    f"\n\tlocation : {ind_msg}"
+                )
+                continue
             for shot in range(shots_per_measurement):
                 tot_bins = drop_bins + ro_bins
                 iteration = int(iteration)
                 measurement = int(measurement)
-                binned_counter_data[iteration, measurement, shot, :] = raw_data[shot*tot_bins: (shot + 1) * tot_bins]
+                if measurement < filter_offset:
+                    print(f"measurement {measurement} has been dropped due to fist-N-filter. First {filter_offset} being dropped")
+                    continue
+                try:
+                    binned_counter_data[iteration, measurement - filter_offset, shot, :] = raw_data[shot*tot_bins: (shot + 1) * tot_bins]
+                # Catch issues reshaping raw_data and omit that shot from binned_counter_data
+                except (IndexError, ValueError) as e:
+                    inds = {
+                        "iteration": iteration,
+                        "measurement": measurement,
+                        "measurement - filter_offset": measurement - filter_offset,
+                        "shot": shot,
+                        "tot_bins": tot_bins,
+                        "raw_data.shape": raw_data.shape}
+                    print(iteration)
+                    ind_msg = ",".join([f"{var} : {val}" for var, val in inds.items()])
+                    print(f"Warning. Error reading data for {ind_msg}\n {e}")
+                    continue
 
     shot_counter_data = binned_counter_data[..., drop_bins:].sum(3)
+    # Fix holes in data by replacing count data with the minimum acquired value
+    # (registers as 0 atoms)
+    min_at = min(shot_counter_data[np.where(shot_counter_data > -1)])
+    shot_counter_data[np.where(shot_counter_data < 0)] = min_at
 
     return binned_counter_data, shot_counter_data
+
 
 def open_results(path: str, results_file: str = "results.hdf5") -> h5py.File:
     """
@@ -92,3 +130,19 @@ def open_results(path: str, results_file: str = "results.hdf5") -> h5py.File:
     else:
         raise ValueError(f"{full_path} is not a valid hdf5 file")
 
+
+def check_n_filter(results_file: h5py.File) -> int:
+    """
+    Checks the filters settings. Returns an offset if the "Drop First N Measurements Filter" is
+    enabled. Returns the number of measurements being dropped at the beginning
+    Args:
+        results_file: h5file object corresponding to the results.hdf5 file being analyzed
+
+    Returns:
+        Offset: Number of measurements being dropped at the beginning of each iteration. If filter
+            is not enabled, returns 0.
+    """
+
+    enabled = results_file["settings/experiment/first_measurements_filter/enable"][()]
+
+    return enabled * results_file["settings/experiment/first_measurements_filter/N"][()]
